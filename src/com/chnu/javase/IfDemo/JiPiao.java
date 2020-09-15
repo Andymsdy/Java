be842f131e3cc73e2ef758eba753e53ed4dd2d6a
Andymsdy <q515697@gmail.com>
2020-09-15 11:27:09 +0800

    作业

--- src/com/chnu/javase/IfDemo/JiPiao.java
+++ src/com/chnu/javase/IfDemo/JiPiao.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class JiPiao {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("请输入订票日期：");
-        Double data=sc.nextDouble();
+        Double data = sc.nextDouble();
 
         System.out.println("请输入机票定价：");
-        Double price=sc.nextDouble();
+        Double price = sc.nextDouble();
 
         if ((data>=1.1 & data<=1.3)||(data>=5.1 & data<=5.3)||(data>=10.1 & data<=10.7)){
             System.out.println("机票无优惠,定价为："+price);
         }
         else{
-            Double q=0.8*price;
+            Double q = 0.8*price;
             System.out.println("机票有优惠,定价为："+q);
         }
     }
--- src/com/chnu/javase/SwitchDemo/ChengJi.java
+++ src/com/chnu/javase/SwitchDemo/ChengJi.java
@@ -1,4 +1,35 @@
 package com.chnu.javase.SwitchDemo;
 
+import java.util.Scanner;
+
 public class ChengJi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("请输入该学生的成绩：");
+        int grade = sc.nextInt();
+
+        if (grade<0 || grade >100){
+            System.out.println("输入的成绩有误！");
+        }
+        else{
+            switch (grade/10){
+                case 10:
+                case 9:
+                    System.out.println("该学生的成绩为A级");
+                    break;
+                case 8:
+                    System.out.println("该学生的成绩为B级");
+                    break;
+                case 7:
+                    System.out.println("该学生的成绩为C级");
+                    break;
+                case 6:
+                    System.out.println("该学生的成绩为D级");
+                    break;
+                default:
+                    System.out.println("该学生的成绩不及格");
+            }
+        }
+    }
 }
